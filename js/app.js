33851e8819cd7834e6bce82dea91aa26bb578c8c
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-19 17:17:10 +0600

    added all the images in the display

---
+++ js/app.js
@@ -0,0 +1,118 @@
+//select cvs
+const cvs = document.getElementById('bird');
+const ctx = cvs.getContext('2d');
+
+//game vars and consts
+let frames = 0;
+
+//load sprite image
+const sprite = new Image();
+sprite.src = 'img/sprite.png';
+
+//create the background
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0,
+    y: cvs.height - 226,
+
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+//forground
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+const bird = {
+    animation: [
+        { sX: 276, sY: 112 },
+        { sX: 276, sY: 139 },
+        { sX: 276, sY: 164 },
+        { sX: 276, sY: 139 }
+    ],
+    x: 50,
+    y: 150,
+    w: 34,
+    h: 26,
+
+    frame: 1,
+    draw: function () {
+        let bird = this.animation[this.frame];
+
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
+    }
+}
+
+//get reaty message
+const getReady = {
+    sX: 0,
+    sY: 228,
+    w: 173,
+    h: 152,
+    x: cvs.width / 2 - 173 / 2,
+    y: 80,
+
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+}
+
+//game over msg
+const gameOver = {
+    sX: 175,
+    sY: 228,
+    w: 225,
+    h: 202,
+    x: cvs.width / 2 - 225 / 2,
+    y: 90,
+
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+}
+
+// draw
+function draw() {
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+    bg.draw();
+    fg.draw();
+    bird.draw();
+    getReady.draw();
+    gameOver.draw();
+
+}
+
+// update 
+function update() {
+
+}
+
+// loop 
+function loop() {
+    update();
+    draw();
+    frames++;
+
+
+    requestAnimationFrame(loop);
+}
+
+loop();
